1efa149556c09459883f88c7085c63e44f943079
Wesley Rogério S Campos <56031533+WesleyRSC@users.noreply.github.com>
2024-04-27 14:48:16 -0300

    build: add Material components to Shared Modules

--- To-Do-List/src/app/shared/shared.module.ts
+++ To-Do-List/src/app/shared/shared.module.ts
@@ -5,7 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HeaderComponent],
@@ -13,8 +17,26 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     CommonModule,
     BrowserModule,
     NgbModule,
+    HttpClientModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
-  exports:[HeaderComponent]
+  exports: [
+    HeaderComponent,
+    CommonModule,
+    BrowserModule,
+    NgbModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
