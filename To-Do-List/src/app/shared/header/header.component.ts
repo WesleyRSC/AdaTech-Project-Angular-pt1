2cd912fe7fbd463ec3132cc85a648c231847bf6a
Wesley Rogério S Campos <56031533+WesleyRSC@users.noreply.github.com>
2024-04-27 14:49:12 -0300

    feat: personalize header component

--- To-Do-List/src/app/shared/header/header.component.ts
+++ To-Do-List/src/app/shared/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { timer, map, share, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  rxTime: Date = new Date();
+  subscription!: Subscription;
 
+  ngOnInit(): void {
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe((time) => {
+        this.rxTime = time;
+      });
+  }
+  
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
