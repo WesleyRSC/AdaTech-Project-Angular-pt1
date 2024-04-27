4573b397994365b96659ffda2cb4e81853c30fcd
Wesley Rogério S Campos <56031533+WesleyRSC@users.noreply.github.com>
2024-04-27 14:49:45 -0300

    feat: create tasks list in task component

--- To-Do-List/src/app/tasks/tasks.component.ts
+++ To-Do-List/src/app/tasks/tasks.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TasksService } from './tasks.service';
+import { Task } from './interfaces/task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+  tasks: Array<Task> = [];
+  constructor(private taskService: TasksService) {}
 
+  ngOnInit(): void {
+    this.getTasks();
+  }
+
+  getTasks() {
+    this.taskService.getTasks().subscribe({
+      next: (result) => {
+        console.log(result);
+        this.tasks = result;
+      },
+      error: (err) => {
+        alert(err.error);
+      },
+    });
+  }
+
+  addTask() {
+    let task: Task = {
+      title: 'Tomar Remédio',
+      description: 'dipirona 20ml',
+      date: new Date(),
+      _id: undefined
+    };
+    this.taskService.addTask(task).subscribe({
+      next: (result) => {
+        this.getTasks();
+      },
+      error: (err) => {
+        alert(err.error);
+      },
+    });
+  }
+
+  removeTask(task: Task) {
+    console.log(task);
+    this.taskService.removeTask(task._id!).subscribe({
+      next: (result) => {
+        this.getTasks();
+      },
+      error: (err) => {
+        alert(err.error);
+      },
+    });
+  }
 }
