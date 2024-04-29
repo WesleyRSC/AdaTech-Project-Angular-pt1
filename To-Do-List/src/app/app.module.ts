19e4caea0d3291e670561455ac24d09ec7b8c6be
Wesley Rogério S Campos <56031533+WesleyRSC@users.noreply.github.com>
2024-04-29 19:48:05 -0300

    feat: create edit-task component

--- To-Do-List/src/app/app.module.ts
+++ To-Do-List/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { SharedModule } from './shared/shared.module';
-import { EditComponent } from './tasks/components/edit/edit.component';
+import { EditTaskComponent } from './tasks/components/edit-task/edit-task.component';
 import { AddTaskComponent } from './tasks/components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
-    EditComponent,
+    EditTaskComponent,
     AddTaskComponent,
   ],
   imports: [SharedModule],
--- To-Do-List/src/app/tasks/components/edit-task/edit-task.component.ts
+++ To-Do-List/src/app/tasks/components/edit-task/edit-task.component.ts
@@ -1,7 +1,63 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Task } from '../../interfaces/task';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
 @Component({
   selector: 'app-editTask',
   templateUrl: './edit-task.component.html',
   styleUrls: ['./edit-task.component.scss'],
 })
 export class EditTaskComponent implements OnInit {
+  @Input() task!: Task;
+
+  @Output() taskUpdatedEvent = new EventEmitter<Task>();
+  @Output() closeEvent = new EventEmitter();
+
+  editTaskForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.editTaskForm = this.fb.group({
+      title: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+      date: new FormControl(new Date(), Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.task);
+    this.editTaskForm = this.fb.group({
+      title: new FormControl(this.task.title, Validators.required),
+      description: new FormControl(this.task.description, Validators.required),
+      date: new FormControl(new Date(this.task.date), Validators.required),
+    });
+  }
+
+  closeModal() {
+    this.closeEvent.emit();
+  }
+
+  updateTask() {
+
+    console.log(this.editTaskForm)
+    if (!this.editTaskForm.valid) {
+      alert('Incorrect Data');
+      return;
+    }
+    if (this.editTaskForm.value.date.getDate() < new Date().getDate()) {
+      alert('Date cannot be less than the current date');
+      return;
+    }
+
+    let updatedTask: Task = this.editTaskForm.getRawValue();
+    updatedTask._id = this.task._id;
+
+    this.taskUpdatedEvent.emit(updatedTask);
+  }
 }
